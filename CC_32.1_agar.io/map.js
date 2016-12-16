aae4cf0830a74f48612ef32b9d2212ebc2e12cdb
Norsi <natalieannorsi@gmail.com>
2016-12-15 19:49:35 -0800

    Boundaries defined!

--- CC_32.1_agar.io/map.js
+++ CC_32.1_agar.io/map.js
@@ -1,27 +1,41 @@
 function Surroundings() {
   
-  var d = 70;
-  var p1 = d;
-  var p2 = p1+d;
-  var p3 = p2+d;
-  var p4 = p3+d;
-
+  
   this.show = function() {
-  // Draw gray box
-  stroke(153);
-  line(p3, p3, p2, p3);
-  line(p2, p3, p2, p2);
-  line(p2, p2, p3, p2);
-  line(p3, p2, p3, p3);
+  var limits = new Rect(-max_width,-max_height,max_width,-max_height,max_width,max_height,-max_width,max_height);
+  limits.show();
+  //borders
+  ellipse(-max_width,max_height,blob.r,blob.r);
+  ellipse(max_width,max_height,blob.r,blob.r);
+  ellipse(-max_width,-max_height,blob.r,blob.r);
+  ellipse(max_width,-max_height,blob.r,blob.r);
+  //+y axis points "down" relative to start
+  //-y axis points "up" relative to start
+  //-x axis points "left" relative to start, etc.
 
-  // Draw white points
-  stroke(255);
-  point(p1, p1);
-  point(p1, p3); 
-  point(p2, p4);
-  point(p3, p1); 
-  point(p4, p2);
-  point(p4, p4);
   }
 
+}
+
+function Rect(point1x,point1y,point2x,point2y,point3x,point3y,point4x,point4y) {
+  this.p1x = point1x;
+  this.p1y = point1y;
+  this.p2x = point2x;
+  this.p2y = point2y;
+  this.p3x = point3x;
+  this.p3y = point3y;
+  this.p4x = point4x;
+  this.p4y = point4y;
+
+  this.show = function() {
+    fill(255);
+    // Draw gray boundary
+    stroke(153);
+    //Lines will be drawn clockwise!
+    line(this.p1x, this.p1y, this.p2x, this.p2y);
+    line(this.p2x, this.p2y, this.p3x, this.p3y);
+    line(this.p3x, this.p3y, this.p4x, this.p4y);
+    line(this.p4x, this.p4y, this.p1x, this.p1y);
+  }
+  
 }
